52e5a3a3d9ccee804db6e8d10ab6720108d4099b
uzli161022 <uzlifatul0221@gmail.com>
2025-07-02 00:29:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 
-app.use(cors({
-  origin: 'https://frontend-xi-six-52.vercel.app/', // ganti sesuai URL Vercel
-  credentials: true
-}));
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontend-xi-six-52.vercel.app/',
+  credentials: true
+}));
 app.use(express.json());
 
 // Database connection
